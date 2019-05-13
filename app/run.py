b255fe2b0fae05a5c0c0ae06a0ba3437c4ec837a
Keith Sato <keith.sato@towerjazz.com>
2019-05-12 21:11:15 -0700

    Single page QR

--- app/run.py
+++ app/run.py
@@ -38,5 +38,23 @@ def qr():
     
     return render_template("qr.html",qr=qrval), 200, {'Content-Type': 'text/html'}
 
+@app.route('/qrstr', methods=['POST'])
+def qrstr():
+    data = request.form
+    qrstr = 'lic:%s;vin:%s;unit:%s;mfg:%s;model:%s;hp:%s;year:%s;dia:%s;kit:%s' % \
+        (data['lic'],data['vin'],data['unit'],data['engMauf'],data['engModel'],
+        data['engHP'],data['engYear'],data['exhDia'],data['exhKit'])
+    img = qrcode.make(qrstr,
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    buf = BytesIO()
+    img.save(buf)
+    qrval = base64.b64encode(buf.getvalue()).decode('ascii')
+    
+    return qrval
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=9100,debug=False)
